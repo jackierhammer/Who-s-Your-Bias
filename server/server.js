85bdf4eaa19f14cd646ad6a322cfb65aa8fe8b2d
Jackie <jackierhammer@gmail.com>
2022-11-17 20:31:08 -0700

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,2 +1,49 @@
-// this file will start the Apollo server
+// imports
+const express = require('express');
+const path = require('path');
 
+// imports type defs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
+// imports database connection
+const db = require('./config/connection');
+
+// imports apollo server
+const { ApolloServer } = require('apollo-server-express');
+
+// auth helper will need to be created
+// imports authMiddleware from auth
+// const { authMiddleware } = require('./utils/auth');
+
+const app = express();
+// opens local host on port 3001
+const PORT = process.env.PORT || 3001;
+
+// create apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+//   uncomment when auth is created
+//   context: authMiddleware
+});
+// since we are using an Apollo Server 2 version before 3, we don't have to use server.start() first
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+};
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  })
+});
