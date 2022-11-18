a8bdee117cd1f84bbb34e98a938bb4c10abeffc8
Jackie <jackierhammer@gmail.com>
2022-11-17 19:19:21 -0700

    added database connection in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,1 +1,10 @@
-// connection to database goes here
+// imports mongoose package
+const mongoose = require('mongoose');
+
+// connects to database using environmental variable or local connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/biasDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
