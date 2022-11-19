2d39a96e83ff85d2b5b181c4288b246b72644411
Jackie <jackierhammer@gmail.com>
2022-11-18 21:43:04 -0700

    added login functionality

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,26 @@
-// mutations will be defined here
+import { gql } from 'graphql-tag';
+
+// creates mutation for logging in existing users
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+            }
+        }
+    }
+`;
+
+// creates mutation for creating new users
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
