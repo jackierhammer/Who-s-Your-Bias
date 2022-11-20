89ce0d957940a9ae0233ac1404e2cb95f03c1011
Jackie <jackierhammer@gmail.com>
2022-11-19 18:26:46 -0700

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,42 @@
-// this file is for creating apollo client and app function
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// added apollo client import
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
+
+// here we make a new apollo client
+const client = new ApolloClient({
+    request: operation => {
+        // grab token from local storage
+        const token = localStorage.getItem('id_token');
+        operation.setContext({
+            headers: {
+                authorization: token ? `Bearer ${token}` : ''
+            }
+        });
+    },
+    uri: '/graphql'
+});
+
+function App() {
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <>
+                    <Navbar />
+                    <Routes>
+                        <Route exact path='/' element={<Explore />} />
+                        <Route exact path='/biases' element={<Biases />} />
+                        <Route exact path='/profile' element={<Profile />} />
+                        <Route render={() => <h1 className='display-2'>Wrong Page</h1>} />
+                    </Routes>
+                </>
+            </Router>
+        </ApolloProvider>
+    );
+}
+
+export default App;
