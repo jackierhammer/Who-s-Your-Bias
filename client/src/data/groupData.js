371e49ab40a0a9fec83e0a2ef73534eee8110e92
Jackie <jackierhammer@gmail.com>
2022-11-23 13:52:46 -0700

    added additional idol groups to the data set

--- client/src/data/groupData.js
+++ client/src/data/groupData.js
@@ -18,6 +18,30 @@ const groupData = [
     {
         "groupName": "Red Velvet",
         "activeMembers": ["Irene", "Seulgi", "Wendy", "Joy", "Yeri"]
+    },
+    {
+        "groupName": "Dreamcatcher",
+        "activeMembers": ["JiU", "SuA", "Siyeon", "Handong", "Yoohyeon", "Dami", "Gahyeon"]
+    },
+    {
+        "groupName": "ITZY",
+        "activeMembers": ["Yeji", "Lia", "Ryujin", "Chaeryeong", "Yuna"]
+    },
+    {
+        "groupName": "NewJeans",
+        "activeMembers": ["Minji", "Hanni", "Danielle", "Haerin", "Hyein"]
+    },
+    {
+        "groupName": "MAMAMOO",
+        "activeMembers": ["Solar", "Moonbyul", "Wheein", "Hwasa"]
+    },
+    {
+        "groupName": "IVE",
+        "activeMembers": ["Yujin", "Gaeul", "Rei", "Wonyoung", "Liz", "Leeseo"]
+    },
+    {
+        "groupName": "LE SSERAFIM",
+        "activeMembers": ["Chaewon", "Sakura", "Yunjin", "Kazuha", "Eunchae"]
     }
 ];
 
